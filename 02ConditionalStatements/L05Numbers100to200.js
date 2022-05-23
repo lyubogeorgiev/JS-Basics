06972b31100e87d52181e9650be3770e2c0f0fd5
Lyubomir Georgiev <lyubo12georgiev@gmail.com>
2022-05-22 23:20:11 -0600

    adding Lab example 5 Numbers 100 to 200 function.

--- 02ConditionalStatements/L05Numbers100to200.js
+++ 02ConditionalStatements/L05Numbers100to200.js
@@ -1,3 +1,13 @@
 function numbers(input){
+    let number = input[0];
 
+    if (number < 100){
+        console.log("Less than 100");
+    } else if (number >= 100 && number <= 200){
+        console.log("Between 100 and 200")
+    } else {
+        console.log("Greater than 200");
+    }
 }
+
+numbers(["210"]);
